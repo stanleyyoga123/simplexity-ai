3105848b5b0d8a1302cfd0a402d021c422f382c3
Ariya Adinatha <13519048@std.stei.itb.ac.id>
2021-10-08 19:09:52 +0700

    max value & min value ditambahin

--- src/ai/minimax.py
+++ src/ai/minimax.py
@@ -1,3 +1,4 @@
+from math import inf
 import random
 from time import time
 
@@ -17,3 +18,32 @@ class Minimax:
         best_movement = (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
 
         return best_movement
+
+    def action(state):
+        # kumpulan action yang bisa dilakuin
+        pass
+
+    def results(s, a):
+        pass
+
+    def terminal_test(state):
+        # check udah menang atau belom
+        pass
+
+    def utility(state):
+        # hasil nilainya kalau udah menang
+        pass
+
+    def max_value(state):
+        if terminal_test(state):
+            return utility(state)
+        v = -999999
+        for item in action(state):
+            v = max(v, min_value(results(state, item)))   
+
+    def min_value(state):
+        if terminal_test(state):
+            return utility(state)
+        v = 999999
+        for item in action(state):
+            v = max(v, max_value(results(state, item))) 
